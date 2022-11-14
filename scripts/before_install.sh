f4c89da21a209070141728b86f525f0a4d45e254
Boyinapalli Sandeep Dora <61022690+LEAGUEDORA@users.noreply.github.com>
2022-11-14 18:08:22 +0530

    added buildspec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #download node and npm
-curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -. ~/.nvm/nvm.sh
-echo installing node
-yum -y install nodejs
+# curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -. ~/.nvm/nvm.sh
+# echo installing node
+# yum -y install nodejs
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
 if [ -d "$DIR" ]; then
