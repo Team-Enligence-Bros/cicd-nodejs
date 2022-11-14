c61d011d13f98750aecc957ac1ea06a4c9dd26d6
Boyinapalli Sandeep Dora <61022690+LEAGUEDORA@users.noreply.github.com>
2022-11-14 17:25:03 +0530

    added watch to pm2 and removed appstop from hook

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 sudo npm install pm2@latest -g
-pm2 start index.js
+pm2 start index.js --watch
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
