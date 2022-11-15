cf6a9219e2ec6ba5f6abbabb74efa9ac08ed6fae
Boyinapalli Sandeep Dora <61022690+LEAGUEDORA@users.noreply.github.com>
2022-11-15 10:32:43 +0530

    added npm i app_start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ubuntu/express-app
-
+npm i
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/express-app
 
